84ff89b0b2b8486d45e1a45b422e2df2fe53350f
Niklas Kreer <kreergit@posteo.de>
2019-02-23 18:59:27 +0100

    Cycle filters in a separately for extensibility

--- src/filters/filters.js
+++ src/filters/filters.js
@@ -10,21 +10,25 @@ const filters = [
 ];
 var selectedFilter = 0;
 
+function cycleFilter(){
+    // Get the next filter, avoid doing the first one twice
+    if((selectedFilter + 1) >= filters.length){
+        selectedFilter = 0;
+    } else {
+        selectedFilter += 1;
+    }
+
+    nextFilter = filters[selectedFilter];
+    // Apply the filter to the canvas
+    canvas.className = "viewer " + nextFilter;
+
+    // Show the filter name in infoText
+    infoText.innerHTML = nextFilter;
+}
+
 // Listen for key presses in order to react to them
 document.addEventListener('keydown', function(event){
     if(event.keyCode == 70){ //f for Filter
-        // Get the next filter, avoid doing the first one twice
-        if((selectedFilter + 1) >= filters.length){
-            selectedFilter = 0;
-        } else {
-            selectedFilter += 1;
-        }
-
-        nextFilter = filters[selectedFilter];
-        // Apply the filter to the canvas
-        canvas.className = "viewer " + nextFilter;
-
-        // Show the filter name in infoText
-        infoText.innerHTML = nextFilter;
+        cycleFilter();
     }
 });
