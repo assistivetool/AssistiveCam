7828b506cf589f22f213af38f1c74bbb3cb240ab
Niklas Kreer <kreergit@posteo.de>
2019-02-26 13:29:13 +0100

    Render the gui

--- src/gui/gui.js
+++ src/gui/gui.js
@@ -1,1 +1,15 @@
+const gui = document.getElementById("gui");
+
 // Create GUI elements dynamically
+for(i = 0; i < guiConfig.length; i++){
+    var button = document.createElement("button");
+    // Set the image and label
+    button.innerHTML = "<img src=\"" + guiConfig[i]["picture"] + "\" alt=\"" + guiConfig[i]["label"] + "\">";
+    // Attach the configured command
+    button.addEventListener("click", function(event){
+        executeCommand(guiConfig[i]["command"]);
+    });
+    
+    // Add the button to the GUI element
+    gui.appendChild(button);
+}
