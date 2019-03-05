8141c9af948fe8b24d5684b078b4ca61d8ad008a
Niklas Kreer <kreergit@posteo.de>
2019-03-05 11:05:58 +0100

    GUI can now be recreated during runtime

--- src/gui/gui.js
+++ src/gui/gui.js
@@ -1,24 +1,34 @@
 const gui = document.getElementById("gui");
 var guiVisible = false;
 
-// Create GUI elements dynamically
-for(i = 0; i < guiConfig.length; i++){
-    var button = document.createElement("button");
-    // Set the image and label
-    button.innerHTML = "<img src=\"" + guiConfig[i]["picture"] + "\" alt=\"" + guiConfig[i]["label"] + "\">";
-    // Attach the configured command
-    button.setAttribute("onmousedown", "executeCommand(\"" + guiConfig[i]["command"] + "\");");    
+function createGUI(){
+    // Create GUI elements dynamically
+    gui.innerHTML = "";
+    for(i = 0; i < guiConfig.length; i++){
+        event.emit("guiElementCreate", guiConfig[i]);
+        var button = document.createElement("button");
+        // Set the image and label
+        button.innerHTML = "<img src=\"" + guiConfig[i]["picture"] + "\" alt=\"" + guiConfig[i]["label"] + "\">";
+        // Attach the configured command
+        button.setAttribute("onmousedown", "executeCommand(\"" + guiConfig[i]["command"] + "\");");    
 
-    // Add the button to the GUI element
-    gui.appendChild(button);
+        // Add the button to the GUI element
+        gui.appendChild(button);
+    }
+    event.emit("guiCreate");
 }
+
+// Create the GUI on startup
+createGUI();
 
 function toggleGUIVisibility(){
     if(guiVisible){
         gui.className = "invisible";
         guiVisible = false;
+        event.emit("guiOff");
     } else {
         gui.className = "";
         guiVisible = true;
+        event.emit("guiOn");
     }
 }
